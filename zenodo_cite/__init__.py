41eed0169d0ee2fd754c0b7f3b8263fad23667e6
s-weigand <s.weigand.phy@gmail.com>
2020-08-15 12:05:54 +0200

    Added __DOI__ to package

--- zenodo_cite/__init__.py
+++ zenodo_cite/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """Sebastian Weigand"""
 __email__ = "s.weigand.phy@gmail.com"
 __version__ = "0.1.0"
+
+__DOI__ = "10.5281/zenodo.3979791"
+"""ZENODO DOI for the package or a version of it"""
